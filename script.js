25ad1ac34ed6953d20ece9c9462ab7b80c5b160a
Shirisha Ale <167552531+Shirishagithub1@users.noreply.github.com>
2024-06-08 11:39:58 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,130 +1,179 @@
+let arr = [1, 2,['siri', 32] , "dhanu", 'mgu', { name : 'ajJs' , age: "13" }, true, false, null, undefined];
 
-let Televison ={
-    name : "R20",
-    company : "Vu",
-    price : 20000,
-    quality : 1024,
-    color :['silver','blue','white'],
-    myFullName : function(name){
-         return `My name is ${name}`
-    },
-    address:{
-        house : {
-            house1 :203,
-            house2 : 200,
-        },
-        pincode :508211,
-        village :'nakrekal',
+// console.log(arr);
 
+
+let name = arr[2][0].slice(0,3);
+console.log(name);
+
+
+let person =arr[5];
+
+console.log(person);
+
+
+//individual items :
+let name2 = person.age
+console.log(name2);
+
+
+let myName =arr[3];
+
+myName =myName.replace('0',"p")
+
+console.log(myName);
+
+//length
+
+let lenghthOfArray =arr.length;
+
+console.log(lenghthOfArray)
+
+let basic =['a','e','i','o']
+
+
+console.log(basic)
+
+//push :
+
+basic.push('u');
+basic.push('p')
+
+console.log(basic)
+
+//pop : remove the end of the array
+
+basic.pop();
+basic.pop();
+
+basic.pop();
+
+
+console.log(basic);
+
+
+//un-shift : add the begining
+
+basic.unshift('i');
+
+console.log(basic);
+
+
+//shift : remove the from begining
+
+basic.shift();
+
+console.log(basic);
+
+
+let emptyArray= [];
+
+console.log(emptyArray);
+
+emptyArray.push("siri");
+
+console.log(emptyArray);
+
+
+
+let array =['siri','jaanu','ram','siva','dileep'];
+
+for(let i=0; i<array.length; i++){
+    let index = array[i];
+    console.log(index);
+}
+
+
+// for (let names of array){
+// console.log(names)
+// }
+
+
+let vowels =['a','u','i','o','e','o','j','l','b','m'];
+
+let onlyVowels =[];
+let consonents =[];
+let vowelsString ='aeiou'
+for (let letter of vowels){
+    if(vowelsString.includes(letter)){
+        onlyVowels.push(letter)
     }
-};
+    else{
+        consonents.push(letter)
+    }
+
+}
+console.log(onlyVowels);
+console.log(consonents);
 
 
-//Object Destructuring : 
+//aray destructuring:
 
-let {name,company,color, price, quality } = Televison;
-console.log('Television name is :', name, quality);
-
-let myName = Televison.myFullName("Siri");
-let myName1 =Televison.myFullName("Ale");
+let array1 =  ['siri','raju','kajal','samantha'] 
 
 
-let myRoom = Televison.address.village;
-console.log(myRoom);
-console.log(myName, myName1);
+let [name1,name3, ,name4] =array1;
+console.log(name1,name3,name4);
 
 
-//square bracket 
-
-let house = Televison['address']['house']['house1'];
-
-console.log(house);
+let persons =[{name : "sam",age :22,study : "B tech"},{name : "Kaju",age :16,study : "B tech"},{name : "Raku",age :12,study : "B tech"}]
 
 
-//Global object inbuilt method math :
+for (let person of persons ){
 
-let minValue =Math.min(10,80);
+    const {name,age,study} =person;
+    console.log(name,age);
 
-console.log(minValue);
-
-//Max
-
-let maxValue = Math.max(20,80,567);
-
-console.log(maxValue);
-
-
-//square root
-
-let myNumber = 25;
-
-let checksqrt=Math.sqrt(myNumber);
-
-console.log(checksqrt);
-
-let tan =Math.sin()
-
-//Round :fraction value
-
-let pizza =290.07; 
-
-//final price :291only
-
-
-
-let finalPrice = Math.round(pizza);
-
-console.log(`pizza price is ${finalPrice}`);
-
-//floor :
-
-let watch = 3000.64;
-
-let watchPrice = Math.floor(watch);
-
-console.log(watchPrice);
-
-//ceil :
-
-let watch1 = 3000.64;
-
-let watchPrice1 = Math.ceil(watch1);
-
-console.log(watchPrice1);
-
-
-//Randem Number :
-
-let random =Math.random() *6;
-
-
-let floorTheValue = Math.floor(random);
-console.log(floorTheValue);
-
-
-
-
-//DOM Manipulations
-
-function changecolor(event){
-    // let random =Math.random() * 255;
-    // console.log(random);
-
-    let random1 = Math.random() * 255;
-   let actualValue = Math.floor(random1);
-
-   let random2 = Math.random() * 255;
-    let actualValue2 = Math.floor(random2);
-
-    let random3 = Math.random() * 255;
-     let actualValue3 = Math.floor(random3);
-
-    //  console.log(actualValue,actualValue2,actualValue3);
-
-
-    document.body.style.backgroundColor = `rgb(${actualValue}, ${actualValue2}, ${actualValue3})`
-
-    var color ='yellow';
-    event.style.backgroundColor =color;
     
 }
+
+
+//higher order functions:-
+
+function fullName(name){
+    return `my name is ` +name;
+}
+let nameOfGirl = fullName("siri");
+
+console.log(nameOfGirl);
+
+
+// //2nd one
+
+// function fullName (age){
+//     const myAge = age();
+//     console.log(myAge);
+// }
+// let nameOfPerson =fullName(function age(){
+//     return 20;
+
+// });
+
+// console.log(nameOfPerson);
+
+function myOwnName(name){
+    let myName =`my name is  ${name}`;
+
+    return myName;
+}
+let namOfPersons = myOwnName(function second(){
+    console
+});//argument
+
+console.log(namOfPersons);
+
+//3 higher order fun we are going to add argument
+
+function myOwnName1(one){
+    one();
+
+}
+myOwnName1(function one(){  //argument
+    console.log("my name is kajal")
+});
+
+
+
+
+
+
