eeb483888a8db8c4d60f68ca0451fa48f361c070
Shirisha Ale <167552531+Shirishagithub1@users.noreply.github.com>
2024-06-08 11:32:46 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,146 +1,69 @@
-//array
+myname ="siri"; 
 
-let arr =[false,[2,"two"], "siri","it","null","undefined","876987" , { name : "sir",course: "MERN" }];
-  
+for(let i=0; i<=10; i=i+2){
+console.log(myname,i)
 
-console.log(arr);
+}
 
+//even or odd numbers 0-100;
 
+for(let i = 1; i <=10; i++){
+    if(i % 2 == 1){
+    console.log("odd no ",i);
+}
 
-console.log
-
-//Arthematic operator
-
-let x = 5;
-let y = 7;
-let z = x+y;
- checkZ = z;
- console.log(z);
+}
 
 
- let a =800;
- let b =8; 
- let total = a%b;
-  checkTotal = total;
-  console.log(total); 
+//intro to backticks
 
- // substraction
+let firstName ="siri ";
 
- let num = 89;
- let num2 = 9;
-  
- let number = num-num2;
+let lastName = "ale";
 
- checkNumber = number;
-  
- console.log(number)
-
- //multiplication
-
- let n =20;
-
- let n1=90;
-
- let n2=88;
-
- console.log(n*n1)*n2
+let fullName = `Hello..${firstName}${lastName} age ${4}`;
+console.log(fullName);
 
 
- //BODMAS  Rule 
+let number = '10'
+const  multiply=`multiplication of ${number}- ${number *4}`;
 
- let number1 = 20;
-  let number2  = 22;
+console.log(multiply)
 
-  let number3 = 8;
-   let number4 = 14;
+// //tables:
 
-   let total1 = (number1 * number3) - (number2 % number4) +number1;
+// let table =prompt();//taking the input from user
 
-   letTotal1 = total1;
+// for(let i = 1; i<=10; i++){
+//     let tableValue =`${table} *${1} = ${table*i}`;
 
-   console.log(total1);
-
-   let sNum = 3;
-   let sNum1 =3;
-
-   let square = sNum **  sNum1;
-   console.log(square)
-
-  //String concatination.
-   //coercion 
+//     console.log(tableValue);
+// }
 
 
-   let sName ="siri"
-  
-   let sName1 = "A";
-
-   let fullName = sName + sName1;
-   console.log(fullName)
-   //string to number
-
-   let name ="siri";
-
-   let numbers = 20;
-
-   let numbera = 30;
-
-   let over = numbers + numbera;
-
-   let total3  = name + numbers;
-
-
-   console.log(total3);
-
-   let total4 =  name + numbers +numbera;
-   console.log(total4);
-   
-
-   //number to string
-
-   let total5 =  numbers + numbera +name;
-
-   console.log(total5);
-
-   let total6 =numbers - numbera+name;
-
-   console.log(total6);
-
-   let total7= name-numbers -numbera;
-
-   console.log(total7)
-
-   //implicit coercion 
-   let number9 = 9;
-
-   let name9 = "siri";
-
-   let numname =number9 + name9;
-
-   console.log(numname)
- 
-   let  numberExtra = "10";
-   let sum = number9 + numberExtra;
-   console.log(sum)
+//while loop :
 
 
 
-   //number constructor method
-
-   //explicit coercion
-
-   let sum2=number9 + Number(numberExtra);
-
-   console.log(sum2);
+while(i <= 10){
+    if(i % 2 ===0)
+console.log(i);
+i++
+};
 
 
-   let firstname ="Dhanu";
+let num = 1;
 
-   let lastname = 20;
+do{
+    console.log("hey", num)
+    num++
+} while(num <= 15);
 
-   let sub  =firstname + lastname;
 
-   console.log(sub)
 
-   let sub1 = firstname - lastname;
 
-   console.log(sub1)
+
+
+
+
+
