6649291d4f0292e9c264ab86e02b2a78195e2aee
Shirisha Ale <167552531+Shirishagithub1@users.noreply.github.com>
2024-06-08 11:33:26 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,78 +1,8 @@
-let college = "MGU" //Global scope
-
-if(true){
-    let college ="Mahathma gandhi university"; //Block scope
-    console.log(college);
-
+function myName(){
+    let name ="siri"
+    let a =30;
+    let b =10;
+    let total = a + b;
+    console.log(total)
 }
-let college2 = "VDC";  //global scope
-
-console.log(college2)
-
-
-//Diff between var and let and const
-
-var myName = "siri";
-
-console.log(myName)
-
-var myName ="Dhanu";
-
-console.log(myName)
-
-//Hoisting :
-myName = "siri";
-
-var myName;
-
-console.log(myName);
-
-//TZD:
-
-friend ="dhanu";
-
-let friend;
-
-console.log(friend);
-
-const uncle ="uncle name";  //vriable declaration
-console.log(uncle);
-
-//var
-
-var school ="elite"; //global variable
-
-var school ="zphs";
-
-
-//let 
-
-// let card ="king"; //block level variable
-//  card ="queen";
-//  card ="joker"
-
-//  console.log(card);
-
-
-//constant
-
-const car = "BMW";
-
-car ="suzuki"
-
-console.log(car);
-
-
-
-let institute3 = new string("siri");
-
-console.log(institute3);
-
-
-
-
-
-
-
-
-
+myName();
