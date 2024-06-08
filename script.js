3f1ce3112761f207f37a9cdf6607dcea04610a1c
Shirisha Ale <167552531+Shirishagithub1@users.noreply.github.com>
2024-06-08 11:37:54 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,108 +1,198 @@
-//1)Function Decaration
+let heading =document.getElementById('js');
+
+// let content = heading.textContent
+
+// console.log(typeof(content));
+
+// heading.textContent = "mahatma gandhi university"
+
+// heading.style.color ='red';
+
+// set the id
+
+// heading.setAttribute('id' , 'java');
 
 
-function myName(){
-    let name ="siri"
+// String methods :
 
-    console.log(name);
-    let a =2;
-    let b =8;
-    console.log(a+b);
+ let college ='kakatiya junior college';
 
-    for(i=0;i<=3;i++){
-        console.log(`Function Decaration ${i}`)
-    }
-
-}
-myName();
-
-
-//2) Function Expression:
-
-let collegeName = function(){
-    console.log("Function Expression")
-}
-collegeName();
-
-
-
-//3)  arrow function
-
-
-let friend = () => {
-    
-    
-    console.log("welcome to insta") //return 
-}
-friend(); //calling function
-
-
-//arguments and parameters return keyword:
-
-const fullName1 = (secondName , firstName,age=20) => {
-
-
-let fllName = `my Name is : ${firstName} ${secondName} my age is :${age}`;
-
-console.log(fllName)
-}
-fullName1("siri", "Ale");
-fullName1("dhan", "R");
-
-
-//return keyword :
-
-const fullName2 = (secondName , firstName,age=20) => {
-
-
-    let myName = `my Name is : ${firstName} ${secondName} my age is :${age}`;
-    
-    return myName;
-    }
-    let result = fullName2("siri", "Ale");
-    
-
-    console.log(result);
-
-
-// Ex-2:
-
-const fullName3 = (secondName , firstName,age=22) => {
-
-
-    return `my Name is : ${firstName} ${secondName} my age is :${age}`;
-    
-   }
-    let result1 = fullName3("jaanu", "Ale");
-    
-
-    console.log(result1);
-
-
-    //Dom(Document Object Model)
-
-    //Manipulating the html css with help of JS
-
-
-    let myHeading = document.getElementById('js');
-
-    let num = 1;
-function changeName(){
-    let a = 90>30;
-
-    myHeading.textContent ="siri";
+for(let i =0; i<college.length; i++){
+    console.log(college[i]);
 }
 
-// const element = document.getElementById("intro");
 
-// document.getElementById("demo").innerHTML ="Rondem text:" +element.unnerHTML;
+//or
+
+for( let string of college){
+    console.log(string)
+}
+// //index positions :
+
+// college = college[0]
 
 
 
 
-const element = document.getElementById("p");
-
-document.getElementById("demo").innerHTML ='The text is first paragraph' +element[0],innerHTML;
+// heading.textContent = college;
 
 
- 
+// //slice method
+
+// let university ="mahathma gandhi university";
+
+// university = university.slice(9,16);
+
+// heading.textContent = university;
+
+
+// //UpperCase and Lowercse :
+
+// school = "Krishnaveni";
+
+// school = school.toUpperCase();
+
+
+// heading.textContent = school;
+
+// //length of string :
+
+// device = "Computer"
+
+// device =device.length
+
+// heading.textContent = device;
+
+
+// //ascii value
+// //device =device[0].charCodeAt9();
+
+
+// //cancatinate
+// home = "Sweet Home";
+
+// home = home.concat(home);
+
+// heading.textContent = home;
+
+
+// //endswith , startwith
+
+
+// item = "spoon";
+
+// item = item.endsWith("n"); 
+
+// heading.textContent = item;
+
+
+// //incldes
+
+// items = "chair";
+
+// items = items.include('ch');
+
+// heading.textContent = items;
+
+
+// //repeat();
+
+// company ="Atos";
+
+// company = company.repeat(2);
+
+// heading.textContent = company;
+
+
+// //repeat()
+// college =college.repeat(3);
+
+
+
+//replace the letter or  word
+
+// college = college.replace("ka","hg");
+
+// college =college.replaceAll('k',h)
+
+
+// //sub string
+
+// college = college.substring(0,5)
+
+
+
+
+
+//part of string
+// college = college.lastIndexOf('k')
+
+
+
+
+//Split : devide individual words:
+
+// college = college.split(',')
+
+
+// console.log(college);
+
+
+// heading.textContent = college;
+
+
+
+
+//to string
+// let number =100.113;
+// let numberOne = number.toString();
+
+// console.log(numberOne)
+
+
+
+
+
+
+
+
+//number tofixed:
+
+// let num =3;
+// let number =num.toExponential()
+// heading.textContent = number;
+
+
+//to fixed
+
+//toprision:
+
+
+
+//objects :
+
+let mobile = {
+    company :'relame',
+    price :   30000,
+    ram : 6 ,
+    rom :120,
+    color :'silver'
+
+    
+};
+
+console.log(mobile);
+
+//for accessing the specific value
+
+//dot notation
+
+let obj =mobile.company
+
+obj = `My Bike is ${mobile.company} ${mobile.name} its price is ${mobile.price}, its gives the mileage of ${mobile.mileage} per liter`;
+
+heading.textContent = obj;
+
+
+
+
