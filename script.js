02acc083c8b22df7c25e06baa9d535d712854210
Shirisha Ale <167552531+Shirishagithub1@users.noreply.github.com>
2024-06-08 11:40:28 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,179 +1,91 @@
-let arr = [1, 2,['siri', 32] , "dhanu", 'mgu', { name : 'ajJs' , age: "13" }, true, false, null, undefined];
-
-// console.log(arr);
-
-
-let name = arr[2][0].slice(0,3);
-console.log(name);
-
-
-let person =arr[5];
-
-console.log(person);
-
-
-//individual items :
-let name2 = person.age
-console.log(name2);
-
-
-let myName =arr[3];
-
-myName =myName.replace('0',"p")
-
-console.log(myName);
-
-//length
-
-let lenghthOfArray =arr.length;
-
-console.log(lenghthOfArray)
-
-let basic =['a','e','i','o']
-
-
-console.log(basic)
-
-//push :
-
-basic.push('u');
-basic.push('p')
-
-console.log(basic)
-
-//pop : remove the end of the array
-
-basic.pop();
-basic.pop();
-
-basic.pop();
-
-
-console.log(basic);
-
-
-//un-shift : add the begining
-
-basic.unshift('i');
-
-console.log(basic);
-
-
-//shift : remove the from begining
-
-basic.shift();
-
-console.log(basic);
-
-
-let emptyArray= [];
-
-console.log(emptyArray);
-
-emptyArray.push("siri");
-
-console.log(emptyArray);
-
-
-
-let array =['siri','jaanu','ram','siva','dileep'];
-
-for(let i=0; i<array.length; i++){
-    let index = array[i];
-    console.log(index);
-}
-
-
-// for (let names of array){
-// console.log(names)
-// }
-
-
-let vowels =['a','u','i','o','e','o','j','l','b','m'];
-
-let onlyVowels =[];
-let consonents =[];
-let vowelsString ='aeiou'
-for (let letter of vowels){
-    if(vowelsString.includes(letter)){
-        onlyVowels.push(letter)
-    }
-    else{
-        consonents.push(letter)
-    }
+const details =(name) =>{
+    const college =name();
+    const info =`my college name is `+college;
+    return info;
 
 }
-console.log(onlyVowels);
-console.log(consonents);
+let response =details(() =>'MGU');
+console.log(response);
 
 
-//aray destructuring:
+//timer
+//1.set interval 
 
-let array1 =  ['siri','raju','kajal','samantha'] 
+// let number =0;
+
+// setInterval(function(){  //callbackfun
+//     number++
+//     console.log("Hey",number);
+// }, 100);
+// clearInterval(timer);
+
+//2.set timeout:
+
+setTimeout(() =>{
+console.log("hello set timeout")
+},100)
 
 
-let [name1,name3, ,name4] =array1;
-console.log(name1,name3,name4);
 
+const timer  = document.getElementById('timer-count');
 
-let persons =[{name : "sam",age :22,study : "B tech"},{name : "Kaju",age :16,study : "B tech"},{name : "Raku",age :12,study : "B tech"}]
+timer.textContent = 60;
 
+let clearTimer;
 
-for (let person of persons ){
+function startTimer(event) {
 
-    const {name,age,study} =person;
-    console.log(name,age);
+    // data.setAttribute('disabled', true);
 
-    
+    clearTimer = setInterval(() => {
+        let counter = Number(timer.textContent);
+        counter -= 1;
+
+        timer.textContent = counter;
+        
+        if(counter === 0){
+            clearInterval(clearTimer)
+        }
+        
+    }, 1000)
 }
 
+const stopTimer = () => clearInterval(clearTimer);
 
-//higher order functions:-
 
-function fullName(name){
-    return `my name is ` +name;
+//Spread Operator and Rest Operator :
+
+let employee = {
+    name : "Janu",
+    age : 23,
+    study: "BE"
 }
-let nameOfGirl = fullName("siri");
 
-console.log(nameOfGirl);
+let employee2 = {
+    name2 : "dhanu",
+    age2 : 42,
+    study3: "Btech"
+};
 
 
-// //2nd one
-
-// function fullName (age){
-//     const myAge = age();
-//     console.log(myAge);
-// }
-// let nameOfPerson =fullName(function age(){
-//     return 20;
-
-// });
-
-// console.log(nameOfPerson);
-
-function myOwnName(name){
-    let myName =`my name is  ${name}`;
-
-    return myName;
+let Collegues = {
+    ...employee,
+    ...employee2
 }
-let namOfPersons = myOwnName(function second(){
-    console
-});//argument
 
-console.log(namOfPersons);
+console.log(Collegues);
 
-//3 higher order fun we are going to add argument
 
-function myOwnName1(one){
-    one();
-
+const employeeDetails ={
+    name :"keerthi",
+    age : 22,
+    isSheGood :true,
+    technology :['java',"testing"]
 }
-myOwnName1(function one(){  //argument
-    console.log("my name is kajal")
-});
 
+//destructuring:
 
+const {name, ...restOfTheKeys} = employeeDetails;
 
-
-
-
+console.log(name)
+console.log(restOfTheKeys)
